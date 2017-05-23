ded8278c10fe8f1870ab9f6e28524412d14b49de
Carlos Roberto Garcia Junior <carlos-r-g-junior@hotmail.com>
2017-05-23 13:52:33 -0300

    Carregando Cartela no Jogo

--- Bingo/Bingo/Jogo.cs
+++ Bingo/Bingo/Jogo.cs
@@ -15,6 +15,54 @@ namespace Bingo
         public Jogo()
         {
             InitializeComponent();
+            bloquearBtn();
+            cartela();
+        }
+
+        private void bloquearBtn()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+            button15.Enabled = false;
+            button16.Enabled = false;
+        }
+
+        private void cartela()
+        {
+            List<int> cartela = new List<int>();
+            for (int i=0; i<16; i++)
+            {
+                cartela.Add(i + 1);
+            }
+
+            button1.Text = Convert.ToString(cartela[0]);
+            button2.Text = Convert.ToString(cartela[1]);
+            button3.Text = Convert.ToString(cartela[2]);
+            button4.Text = Convert.ToString(cartela[3]);
+            button5.Text = Convert.ToString(cartela[4]);
+            button6.Text = Convert.ToString(cartela[5]);
+            button7.Text = Convert.ToString(cartela[6]);
+            button8.Text = Convert.ToString(cartela[7]);
+            button9.Text = Convert.ToString(cartela[8]);
+            button10.Text = Convert.ToString(cartela[9]);
+            button11.Text = Convert.ToString(cartela[10]);
+            button12.Text = Convert.ToString(cartela[11]);
+            button13.Text = Convert.ToString(cartela[12]);
+            button14.Text = Convert.ToString(cartela[13]);
+            button15.Text = Convert.ToString(cartela[14]);
+            button16.Text = Convert.ToString(cartela[15]);
         }
 
         private void button1_Click(object sender, EventArgs e)
